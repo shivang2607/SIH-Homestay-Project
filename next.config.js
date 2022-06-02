573d307ecbc14ae1621536e0358753da88f7fe27
Akshat <akshatgauriiitsurat@gmail.com>
2022-06-02 19:30:43 +0530

    akshat

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "dns":false,
+        "child_process":false,
+        "tls": false
+      }
+    }
+    return config
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css'
-import NavBar from '../components/navbar';
-import Footer from '../components/footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
+import { FirebaseProvider } from "../context/firebaseContext";
 
 function MyApp({ Component, pageProps }) {
-
-  return <>
-  <NavBar/>
-   <Component {...pageProps} />
-   <Footer/>
-   </>
+  return (
+    <>
+    <FirebaseProvider>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+      </FirebaseProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
