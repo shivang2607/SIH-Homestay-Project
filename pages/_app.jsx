cbba674d4b9a14d895ff293578c171b020b77adc
Akshat <akshatgauriiitsurat@gmail.com>
2022-06-04 17:54:02 +0530

    updated _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,8 @@ import "../styles/globals.css";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import { FirebaseProvider } from "../context/firebaseContext";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Steps } from "chakra-ui-steps";
 
 const theme = extendTheme({
   components: {
@@ -11,16 +12,15 @@ const theme = extendTheme({
   },
 });
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <FirebaseProvider>
-      <ChakraProvider theme={theme} >
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
+      <ChakraProvider theme={theme}>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
       </ChakraProvider>
-      </FirebaseProvider>
+    </FirebaseProvider>
   );
 }
 
